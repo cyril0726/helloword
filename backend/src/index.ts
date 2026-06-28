aa72d97f21bc90a0497e9a024f31a31c0f786d2f
Cyril <cyrilgourdon.cg@gmail.com>
2026-06-28 16:21:18 +0200

    testtt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,6 +20,11 @@ export default {
       return new Response("Worker API running 🚀")
     }
 
-    return new Response('Not found', { status: 404 })
+    return new Response(JSON.stringify({
+  pathname: url.pathname,
+  url: request.url
+  }), {
+    headers: { 'Content-Type': 'application/json' }
+  })
   }
 }
