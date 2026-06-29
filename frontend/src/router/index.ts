0a2b55087c4ba0ab0ce2e85346b1453af6133eb9
Cyril <cyrilgourdon.cg@gmail.com>
2026-06-29 11:50:27 +0200

    retrait d'import inutilise qui bloque le build

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,27 +1,9 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
-
+import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
-
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import guestbook from '../views/GuestbookView.vue'
-
-/*const routes: RouteRecordRaw[] = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
-  { path: '/guestbook', name: 'guestbook',  component: guestbook }
-]*/
 
 const routes = [
   { path: '/', component: Dashboard }
 ]
-
-/*const router = createRouter({
-  history: createWebHistory(),
-  routes
-})*/
-
-
 
 export default createRouter({
   history: createWebHistory(),
